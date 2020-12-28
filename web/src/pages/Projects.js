c14385a2faee27c793c43e700741025e7078a7b0
Felipe Souza <felipelsouza.dev@gmail.com>
2020-12-28 19:40:40 -0300

    web: Create Logout

--- web/src/pages/Projects.js
+++ web/src/pages/Projects.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
+
+import { logout } from '../services/auth'
 
 function Projects() {
+    const history = useHistory();
+
+    function handleLogout() {
+        logout();
+        history.push('/');
+    }
+
     return (
-        <div>Projetos</div>
+        <>
+            <div>Projetos</div>
+            <button onClick={handleLogout}>LOGOUT</button>
+        </>
     );
 };
 
